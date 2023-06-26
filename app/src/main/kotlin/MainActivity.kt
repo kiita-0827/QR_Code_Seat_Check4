661d2a2542de7e57ff34f820bcf57dfad04e7f12
yusuke higashi <higaora.com@gmail.com>
2023-06-26 15:06:52 +0900

    test

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -1,54 +1,81 @@
 import android.Manifest
 import android.content.pm.PackageManager
-import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraManager: CameraManager
+    private var cameraDevice: CameraDevice? = null
 
-    val R = resources
+    private val cameraStateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            // カメラがオープンされた時の処理を追加する
+        }
 
-    companion object {
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        override fun onDisconnected(camera: CameraDevice) {
+            cameraDevice?.close()
+            cameraDevice = null
+            // カメラが切断された時の処理を追加する
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            cameraDevice?.close()
+            cameraDevice = null
+            // カメラでエラーが発生した時の処理を追加する
+        }
     }
 
-    private var cameraManager: CameraManager? = null
+    private val surfaceHolderCallback = object : SurfaceHolder.Callback {
+        override fun surfaceCreated(holder: SurfaceHolder) {
+            openCamera()
+        }
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            // サーフェスが変更された時の処理を追加する
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
+            cameraDevice?.close()
+            cameraDevice = null
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        cameraManager = getSystemService(CAMERA_SERVICE) as? CameraManager
+        val surfaceView: SurfaceView = findViewById(R.id.surfaceView)
+        surfaceView.holder.addCallback(surfaceHolderCallback)
 
-        requestCameraPermission()
-    }
+        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
 
-    private fun requestCameraPermission() {
+        // カメラのパーミッションをリクエストする
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
-        } else {
-            openCamera()
         }
     }
 
     private fun openCamera() {
         try {
-            val cameraId = cameraManager?.cameraIdList?.get(0)
-            cameraManager?.openCamera(cameraId, null, null)
+            val cameraId = cameraManager.cameraIdList[0] // カメラのIDを選択する
+            cameraManager.openCamera(cameraId, cameraStateCallback, null)
         } catch (e: CameraAccessException) {
             e.printStackTrace()
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openCamera()
-            }
-        }
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
     }
 }
